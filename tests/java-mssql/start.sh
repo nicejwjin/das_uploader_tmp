1d7176ff9aeefbc61513a3320a0aae76c868106f
jwjin <jwjin@me.com>
2016-09-30 10:47:36 +0900

    test

--- tests/java-mssql/start.sh
+++ tests/java-mssql/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 javac MsSQL.java
-java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=dasjeongseon;password=dasjeongseon123;database=jeongseon2012" "select top 1 * from tbcb_board_file;"
+java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=jeongseon;password=elzb00;database=jeongseon2012" "select * from tbcb_board_file;"
