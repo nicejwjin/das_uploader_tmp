f194ea7db104c34efbe7e85f8cc8fcd2c5d00378
jwjin <jwjin@me.com>
2016-09-30 10:51:52 +0900

    test

--- tests/java-mssql/MsSQL.java
+++ tests/java-mssql/MsSQL.java
@@ -8,19 +8,26 @@ public class MsSQL {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		//System.out.println(args.length);
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		Connection conn = DriverManager.getConnection(args[0]);
-		Statement sta = conn.createStatement();
-		String Sql = args[1];
-		ResultSet rs = sta.executeQuery(Sql);
-		if(rs.next()) {
-		    System.out.println(rs.getString(1));
-		}
+		try {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection conn = DriverManager.getConnection(args[0]);
+            Statement sta = conn.createStatement();
+            String Sql = args[1];
+            ResultSet rs = sta.executeQuery(Sql);
+            if(rs.next()) {
+                System.out.println(rs.getString(1));
+            }
 
-		//while (rs.next()) {
-		//	System.out.println(rs.getString(1));
-		//}
+            //while (rs.next()) {
+            //	System.out.println(rs.getString(1));
+            //}
+        }
+        catch(Exception e) {
+            System.out.println(e.toString());
+            exit(1);
+        }
+        exit(0);
 	}
 }
--- tests/java-mssql/start.sh
+++ tests/java-mssql/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 javac MsSQL.java
-java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=jeongseon;password=elzb00;database=jeongseon2012" "select * from tbcb_board_file;"
+java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=jeongseon;password=elzb00;database=jeongseon2012" "select * from TBCB_BOARD_FILE;"
