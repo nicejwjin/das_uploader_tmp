5691c1753e219f1f3f36ef504d9fcc1a75a58d5c
jwjin <jwjin@me.com>
2016-09-30 10:54:49 +0900

    test

--- tests/java-mssql/start.sh
+++ tests/java-mssql/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 javac MsSQL.java
-java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=jeongseon2012;password=elzb00;database=jeongseon2012" "select * from TBCB_BOARD_FILE;"
+java MsSQL "jdbc:sqlserver://152.99.171.251:1433;user=jeongseon2012;password=elzb00;database=jeongseon2012" "select * from TBCD_BOARD_FILE;"
